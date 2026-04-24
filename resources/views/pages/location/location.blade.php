3b455f7dd000aa0ed6c07572b6d890fdaf70f0d9
Saumy Anand <samcool3203@gmail.com>
2026-04-24 18:39:44 +0530

    location page updated

--- resources/views/pages/location/location.blade.php
+++ resources/views/pages/location/location.blade.php
@@ -450,13 +450,12 @@
             </article>
 
             <article class="lg:col-span-7 rounded-2xl border border-subtle bg-[#0b0d0f] overflow-hidden">
-                <iframe
-                    title="Tobac-Go Hookah Shop in Noida Map"
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.049782791698!2d77.38031990974697!3d28.56826777559849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cef9220724b87%3A0x21576299f7843f22!2sTobac-go!5e0!3m2!1sen!2sin!4v1777036091183!5m2!1sen!2sin"   title="Tobac-Go Hookah Shop in Noida Map"
                     class="w-full h-[340px] sm:h-[420px]"
                     loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"
-                    src="https://www.google.com/maps?q=Shop+No.+38-39,+Street+76+Market,+Amarpali+Silicon+City,+Sector+76,+Noida&output=embed">
-                </iframe>
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+               
             </article>
         </div>
     </section>
