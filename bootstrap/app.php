f40d1a7bda4eecd115d26326060f1382b2cc06d4
Saumy Anand <samcool3203@gmail.com>
2026-06-15 12:03:16 +0530

    feat: initialize application bootstrap configuration with custom middleware, CSRF exclusions, and route definitions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
 
         $middleware->validateCsrfTokens(except: [
             'payment/phonepe/callback',
